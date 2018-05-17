c0928f4e99edd7492b068d48091ca028956aaa0f
Ramin Keene <raminkeene@gmail.com>
2018-05-16 20:40:36 -0700

    go config

--- customizations/go-mode.el
+++ customizations/go-mode.el
@@ -1,1 +1,8 @@
 (add-hook 'before-save-hook 'gofmt-before-save)
+
+(add-hook 'go-mode-hook
+          (lambda ()
+            (setq-default)
+            (setq tab-width 4)
+            (setq standard-indent 4)
+))
